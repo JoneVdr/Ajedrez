de4f9758d405bbf2b67d30c50eca4e0f4cb647c8
jone <jvegadel@myuax.com>
2022-12-12 16:16:56 +0100

    d

--- codigo.py
+++ codigo.py
@@ -1,5 +1,8 @@
 def partida_ajedrez(nombre_fichero):
-    fichero = open(nombre_fichero, 'r')
-    partida = fichero.read()
-    fichero.close()
-    return partida
+    tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
+    print(tablero_inicial)
+    tablero = []
+    for i in tablero_inicial.split('\n'):
+        tablero.append(i.split('\t'))
+    print(tablero)
+    f = open(nombre_fichero, 'w')
