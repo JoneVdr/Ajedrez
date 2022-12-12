d9415e0c29a188cb96a2bd783094a51729d975c7
jone <jvegadel@myuax.com>
2022-12-12 16:26:56 +0100

    f

--- codigo.py
+++ codigo.py
@@ -21,4 +21,13 @@ while True:
         columna_origen = int(input('Introduce la columna de la pieza que quiere mover: '))
         fila_destino = int(input('Introduce la fila de destino: '))
         columna_destino = int(input('Introduce la columna de destino: '))
+        tablero[fila_destino-1][columna_destino-1] = tablero[fila_origen-1][columna_origen-1]
+        tablero[fila_origen-1][columna_origen-1] = '\t'
+        movimientos += 1
+        f = open('partida.txt', 'a')
+        f.write('Movimiento' + str(movimiento) + '\n')
+        for i in tablero:
+            f.write('\t'.join(i) + '\n')
+        f.close()
+return
 
