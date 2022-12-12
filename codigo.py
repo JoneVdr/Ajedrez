0f79cf9aa704c883a968dc5acda2097440ae51a5
jone <jvegadel@myuax.com>
2022-12-12 16:55:33 +0100

    d

--- codigo.py
+++ codigo.py
@@ -1,16 +1,12 @@
 def partida_ajedrez(nombre_fichero):
     tablero_inicial = '♜\t♞\t♝\t♛\t♚\t♝\t♞\t♜\n♟\t♟\t♟\t♟\t♟\t♟\t♟\t♟\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\n♙\t♙\t♙\t♙\t♙\t♙\t♙\t♙\n♖\t♘\t♗\t♕\t♔\t♗\t♘\t♖'
-    print(tablero_inicial)
     tablero = []
     for i in tablero_inicial.split('\n'):
         tablero.append(i.split('\t'))
-    print(tablero)
     f = open(nombre_fichero, 'w')
     for i in tablero:
         f.write('\t'.join(i) + '\n')
-        f.close()
-    return tablero
-
+    f.close()
     movimiento = 0
     while True:
         continuar = input('¿Desea continuar? (s/n): ')
